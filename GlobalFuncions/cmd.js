cd68e663db606c204f5d73c57e79e1a3e7949006
Sérgio Glórias <sergioglorias@gmail.com>
2019-09-19 22:03:42 +0100

    +mais alguns ajutes

--- GlobalFuncions/cmd.js
+++ GlobalFuncions/cmd.js
@@ -5,13 +5,13 @@ module.exports = {
     //usado para registrar comandos ao iniciar o bot
     register: async (cmd, file, db) => {
         async function verify() {
-           return await db.ref(`GlobalConfig/commands/${file.split('.')[0]}`).once("value")
+           return await db.ref(`GlobalConfig/commands/${file.split('.')[0]}/${file.split('.')[1]}`).once("value")
                 .then(async (snap) => {
                     return snap.val()
                 })
                 .catch(async (e) => {
-                    console.log(`[${tags.ERROR}] `)
-                    console.log(e)
+                    console.log(`[${tags.ERROR}] falha ao verificar o ${file.split('.')[0]}`)
+                    console.log(e.message)
                     return process.exit()
                 })
         }
@@ -19,14 +19,17 @@ module.exports = {
         let data = await verify()
 
         if ((data) == null) {
-            await db.ref(`GlobalConfig/commands/${file.split('.')[0]}`)
+            await db.ref(`GlobalConfig/commands/${file.split('.')[0]}/${file.split('.')[1]}`)
                 .set({
                     ativate: "false",
                     reason: "null"
                 })
-                .then(async () => data = await verify())
+                .then(async () => {
+                    console.log(`[${tags.INFO}] Comando: ${file.split('.')[1]}} registrado`)
+                    data = await verify()
+                })
                 .catch(async (e) => {
-                    console.log(`Comando: ${file} falhou ao registrar na firebase!!`)
+                    console.log(`[${tags.ERROR}] Comando: ${file.split('.')[1]} falhou ao registrar na firebase!!`)
                     console.log(e.message)
                     return process.exit()
                 })
@@ -38,12 +41,13 @@ module.exports = {
             run: cmd.run,
             help: cmd.help,
             config: cmd.config,
+            type: file.split('.')[0],
             block: {
                 ativate: data.ativate,
                 reason: data.reason
             }
         }
 
-        return console.log(`[${tags.SUCCESS}]Comando: ${file} ativado!!`)
+        return console.log(`[${tags.SUCCESS}] Comando: ${file.split('.')[1]} ativado!!`)
     }
 }

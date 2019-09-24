7bc0f9f48e5e914986e612a1e77ddd1d9ff28003
Sérgio Glórias <sergioglorias@gmail.com>
2019-09-24 22:10:17 +0100

    sistema de setPresence adicionado

--- events/once/ready.js
+++ events/once/ready.js
@@ -1,6 +1,19 @@
 /*global tags*/
 /*eslint no-undef: "error"*/
+const RPCs = require("../../data/precense.json")
 
 module.exports = async (client) => {
     console.log(`[${tags.SUCCESS}] ${client.user.username} fui iniciado com sucesso!!`)
+
+    precence(0)
+
+    async function precence(i) {
+        await client.user.setPresence(RPCs[i])
+
+        setTimeout(async () => {
+            let u = i + 1
+            if (u === RPCs.length) u = 0
+            precence(u)
+        }, 30 * 1000)
+    }
 }

a5807611bf3d03d5d8381279bc7808245d64988e
Sérgio Glórias <sergioglorias@gmail.com>
2019-10-16 14:17:50 +0000

    esqueci de fazer controlo se não tiver nada

--- commands/music.playing.js
+++ commands/music.playing.js
@@ -19,7 +19,11 @@ module.exports = {
     // eslint-disable-next-line no-unused-vars
     run: async (client, message, args, cmd) => {
         try {
-            message.reply(await playingNow[message.guild.id].mode(message.guild.id))
+            if (!playingNow[message.guild.id] || !playingNow[message.guild.id].mode) {
+                 message.reply("eu não estou tocando nenhuma musica!!")
+            } else {
+                message.reply(await playingNow[message.guild.id].mode(message.guild.id))
+            }
         } catch (error) {
             CMDs.erro(client, message, cmd, error)
         } finally {

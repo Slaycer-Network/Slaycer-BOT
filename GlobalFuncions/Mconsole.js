f0895a5aa2032bd2c7902d601876e55b33be65ed
Sérgio Glórias <sergioglorias@gmail.com>
2019-10-22 14:35:00 +0000

    sistema de mensagens da database feito
    agora só falta aplicar

--- GlobalFuncions/Mconsole.js
+++ GlobalFuncions/Mconsole.js
@@ -9,13 +9,48 @@ let mgsNull = colorNull(mConsole.Null)
 let yellow = clc.yellowBright
 let green = clc.greenBright
 
+//tags e a usas cores
+async function colorTags (mgs) {
+    let cT = {
+        warning :   clc.yellowBright(`[${tags.WARNING}]`),
+        error   :   clc.redBright(`[${tags.ERROR}]`),
+        success :   clc.greenBright(`[${tags.SUCCESS}]`),
+        info    :   clc.blueBright(`[${tags.INFO}]`)
+    }
+    return await mgs.replace("{WARNING}", cT.warning)
+                    .replace("{ERROR}", cT.error)
+                    .replace("{SUCCESS}", cT.success)
+                    .replace("{INFO}", cT.info)
+}
+
 module.exports = {
     shardManager: async (name, shard, manager) => {
         const { shardManager } = mConsole
 
         if(!shardManager[name]) return mgsNull
-        else return shardManager[name].slice().replace("{SM}", yellow("[ShardingManager]"))
-                                              .replace("{shard}", green(`${shard.id + 1}`))
-                                              .replace("{shardTotal}", green(`${manager.totalShards}`))
+        else return await colorTags(shardManager[name].slice()).replace("{SM}", yellow("[ShardingManager]"))
+                                                               .replace("{shard}", green(`${shard.id + 1}`))
+                                                               .replace("{shardTotal}", green(`${manager.totalShards}`))
+    },
+
+    cmd: async (name, shardId) => {
+
+    },
+
+    database: async (name, shardId) => {
+        const { database } = mConsole
+        let shardTag = clc.cyan(`[Shard-${shardId}]`)
+
+        if(!database[name]) return mgsNull
+        else if (Array.isArray(database[name])) {
+            let mgs = []
+            for (let i in database[name]) {
+                let u = `${shardTag}${await colorTags(database[name][i].slice())}`
+                mgs.push(u)
+            }
+            return mgs.join("\n")
+        } else {
+            return `${shardTag}${await colorTags(database[name].slice())}`
+        }
     }
 }

8d52c5e9fd5f36d06b0a379241f47d27ad932429
Sergio <sergioglorias@gmail.com>
2020-01-08 19:02:33 +0000

    update yarn and add funcion search

--- commands/CMDFuncions/search.js
+++ commands/CMDFuncions/search.js
@@ -3,6 +3,17 @@ const { URLSearchParams } = require("url");
 
 module.exports = {
     search: async (client, search) => {
-        
+        const node = client.player.nodes.first()
+
+        const params = new URLSearchParams()
+        params.append("identifier", search)
+
+        return fetch(`http://${node.host}:${node.port}/loadtracks?${params.toString()}`, { headers: { Authorization: node.password } })
+        .then(res => res.json())
+        .then(data => data.tracks)
+        .catch(err => {
+            console.error(err);
+            return null;
+        });
     }
 }

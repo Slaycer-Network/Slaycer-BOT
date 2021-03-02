cad674d5ff26a07b3d2660e45c44c5e172ab290d
Sergio Glorias <sergioglorias@gmail.com>
2021-03-02 19:54:32 +0000

    save back

--- events/shardManager/spawn.js
+++ events/shardManager/spawn.js
@@ -1,16 +1,12 @@
 /*global webhookAlerts*/
 /*eslint no-undef: "error"*/
-const
-    locales = require("../../locales/console/shardManerger.js"),
-    { hookErrorAlert } = webhookAlerts.erro,
-    {  } = webhookAlerts.shardManerger
     
 module.exports = async (shard, _process) => {
     console.log(locales.shardSpawn(shard.id))
 
     _process.stderr.setEncoding("utf-8")
     _process.stderr.on("data", (log) => {
-        hookErrorAlert(log, shard.id)
+        
     })
 
 }
--- events/webhooksAlerts/error.js
+++ events/webhooksAlerts/error.js
@@ -1,37 +1,8 @@
 const 
     { WebhookClient, MessageEmbed } = require("discord.js"),
-    { errorCode } = require("../configs/tokens.js").discord.webhooksAlets,
-    webLocale = require("../locales/webhook/error.js"),
-    consoleLocale = require("../locales/console/webhook.js")
+    tokenLogin = require("../../configs/tokens.js")
 
-const webhook = new WebhookClient(errorCode.hookID, errorCode.token)
 
-async function hookErrorAlert(erro = "", shardID) {
-    const embed = new MessageEmbed()
-        .setTitle(webLocale.embed.title(shardID))
-        .setColor("RED")
-        .setTimestamp()
-        .setFooter(webLocale.embed.footer)
-    
-    let messageERR
-    if (erro.length > 2000) messageERR = erro.slice(-2000)
-    else messageERR = erro
-
-    embed.setDescription(`\`\`\`\n${messageERR}\n\`\`\``)
-    
-    try {
-        await webhook.send({
-            embeds: [
-                embed
-            ]
-        })
-        console.log()
-    } catch (error) {
-        
-    }
-    
-}
-
-module.exports = {
-    hookErrorAlert
+module.exports = async (shardID, err) => {
+    const webhook = new WebhookClient()
 }
